cfc8c6ee9b1ad592e28a053d0dc6072a81e2f659
Pawel Psztyc <jarrodek@gmail.com>
2021-01-12 21:23:01 -0800

    chore: adding response file save
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- src/ApiResponseViewElement.d.ts
+++ src/ApiResponseViewElement.d.ts
@@ -1,8 +1,26 @@
 import { CSSResult, TemplateResult } from 'lit-element';
 import { ResponseViewElement } from '@advanced-rest-client/arc-response';
+import { ArcExportFilesystemEvent } from '@advanced-rest-client/arc-events';
+
+export declare const saveFileHandler: unique symbol;
 
 export declare class ApiResponseViewElement extends ResponseViewElement {
   get styles(): CSSResult;
+
+  constructor();
+
+  connectedCallback(): void;
+
+  disconnectedCallback(): void;
+
+  [saveFileHandler](e: ArcExportFilesystemEvent): void;
+
+  /**
+   * @param data The exported data 
+   * @param mime The data content type
+   * @param file The export file name
+   */
+  downloadFile(data: BlobPart, mime: string, file: string): void;
 
   render(): TemplateResult;
 }
--- src/ApiResponseViewElement.js
+++ src/ApiResponseViewElement.js
@@ -2,11 +2,59 @@
 import { html } from 'lit-element';
 import { ResponseViewElement } from '@advanced-rest-client/arc-response';
 import { responseTemplate, responseHeadersTemplate, urlStatusTemplate } from '@advanced-rest-client/arc-response/src/internals.js';
+import { DataExportEventTypes } from '@advanced-rest-client/arc-events';
 import elementStyles from './styles/Response.styles.js';
+
+/** @typedef {import('@advanced-rest-client/arc-events').ArcExportFilesystemEvent} ArcExportFilesystemEvent */
+
+export const saveFileHandler = Symbol('saveFileHandler');
 
 export class ApiResponseViewElement extends ResponseViewElement {
   get styles() {
     return elementStyles;
+  }
+
+  constructor() {
+    super();
+    this[saveFileHandler] = this[saveFileHandler].bind(this);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener(DataExportEventTypes.fileSave, this[saveFileHandler]);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.removeEventListener(DataExportEventTypes.fileSave, this[saveFileHandler]);
+  }
+
+  /**
+   * @param {ArcExportFilesystemEvent} e
+   */
+  [saveFileHandler](e) {
+    const { providerOptions, data } = e;
+    const { file, contentType='text/plain' } = providerOptions;
+    this.downloadFile(data, contentType, file);
+  }
+
+  /**
+   * @param {BlobPart} data The exported data 
+   * @param {string} mime The data content type
+   * @param {string} file The export file name
+   */
+  downloadFile(data, mime, file) {
+    const a = document.createElement('a');
+    const blob = new Blob([data], { type: mime });
+    const url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = file;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);  
+    }, 0); 
   }
 
   render() {
